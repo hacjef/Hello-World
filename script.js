35d06a865dc5e1c2fd1364653405d043aea58de9
hacjef <jefferson.canamo21@gmail.com>
2025-01-17 16:03:56 +0800

    adding es6

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function myFunction() {
     alert("I am an alert box!");
   }
+
+
+  const x = (x, y) => x * y;
+  document.getElementById("demo").innerHTML = x(5, 5);
+
+  // Create an Object
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  
+  // Destructuring Assignment
+  let { firstName, age } = person;
+  
+  // Display Data
+  document.getElementById("demo2").innerHTML =
+  firstName + " is " + age + " years old.";
