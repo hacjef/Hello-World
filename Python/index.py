c13604470be7ce60b99ef95859b2affb216adc7c
hacjef <jefferson.canamo21@gmail.com>
2025-01-18 16:15:20 +0800

    sample python

---
+++ Python/index.py
@@ -0,0 +1,23 @@
+print("Hello, World!")
+
+x = 30
+y = 30
+
+print(x + y)
+
+car = {"honda","kia","toyota"}
+
+car.add("bmw")
+
+print(car)
+
+if x > y :
+    print("x is greater than y")
+elif x == y:
+    print("x and y is equal")
+
+print("What is youre name?")
+
+name = input()
+
+print('hi po ' + name)  
